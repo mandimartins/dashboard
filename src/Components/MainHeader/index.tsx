04c34fe3db6df87778440a42336a439caa43f905
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-08-27 12:38:57 -0300

    Add emojis

--- src/Components/MainHeader/index.tsx
+++ src/Components/MainHeader/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Container, Profile, Welcome, UserName } from './styles';
 
+import emojis from '../../utils/emojis';
+
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const indice = Math.floor(Math.random() * emojis.length);
+    return emojis[indice];
+  }, []);
+
   return (
     <Container>
       <h1>Toggle</h1>
 
       <Profile>
-        <Welcome>Olá,</Welcome>
+        <Welcome>Olá,{emoji}</Welcome>
         <UserName>Amanda Martins</UserName>
       </Profile>
     </Container>
