36a260ce710e79a7d8b297a6d2c6c437ea2ccf82
Amanda Martins <47390004+mandimartins@users.noreply.github.com>
2020-09-14 13:36:05 -0300

    Style scrollbar

--- src/Components/Content/styles.ts
+++ src/Components/Content/styles.ts
@@ -5,4 +5,21 @@ export const Container = styled.div`
   color: ${(props) => props.theme.colors.white};
   background-color: ${(props) => props.theme.colors.primary};
   padding: 2.5rem;
+
+  height: calc(100vh - 70px);
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 1rem;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.colors.secondary};
+    border-radius: 1rem;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${(props) => props.theme.colors.tertiary};
+    border-radius: 1rem;
+  }
 `;
