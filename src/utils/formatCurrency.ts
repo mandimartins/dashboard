38010904d71ce446d058124fb012a3d5c10c45e0
mandimartins <mandimartins23@gmail.com>
2021-04-10 14:20:09 -0300

    fix: wrong config

--- src/utils/formatCurrency.ts
+++ src/utils/formatCurrency.ts
@@ -1,5 +1,8 @@
 const formatCurrency = (current: number): string => {
-  return current.toLocaleString("pt-br", { style: "current", currency: "BRL" });
+  return current.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
 };
 
 export default formatCurrency;
